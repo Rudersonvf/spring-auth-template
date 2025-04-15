685885c4bb0b75336ad758daf9836111f46b003a
Ruderson Florentino <contato@ruderson.com.br>
2025-04-15 10:15:49 -0300

    first commit

---
+++ src/main/java/br/com/ruderson/spring_auth_template/SpringAuthTemplateApplication.java
@@ -0,0 +1,13 @@
+package br.com.ruderson.spring_auth_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ruderson/spring_auth_template/SpringAuthTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ruderson.spring_auth_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
